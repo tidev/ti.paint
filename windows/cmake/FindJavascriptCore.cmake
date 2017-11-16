97be454ba09bb9a302cfd40b0b15ea07d970215f
Kota Iguchi <developer@infosia.co.jp>
2017-11-16 11:37:13 +0900

    Bump version to 3.0.0, update copyright

--- windows/cmake/FindJavascriptCore.cmake
+++ windows/cmake/FindJavascriptCore.cmake
@@ -1,7 +1,7 @@
 # FindJavaScriptCore
 # Author: Chris Williams
 #
-# Copyright (c) 2014 by Appcelerator, Inc. All Rights Reserved.
+# Copyright (c) 2017 by Axway All Rights Reserved.
 # Licensed under the terms of the Apache Public License.
 # Please see the LICENSE included with this distribution for details.
 
