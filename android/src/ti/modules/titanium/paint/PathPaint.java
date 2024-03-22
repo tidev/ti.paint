332026db071ba037c60d37cb404f8cf9a27acbf6
m1ga <migamiga@web.de>
2024-03-22 22:07:00 +0100

    add fill method

--- android/src/ti/modules/titanium/paint/PathPaint.java
+++ android/src/ti/modules/titanium/paint/PathPaint.java
@@ -11,50 +11,50 @@ import org.appcelerator.titanium.util.TiConvert;
 
 public class PathPaint {
 
-	private Path myPath;
-	private Paint myPaint;
-	private Boolean isErease = false;
+    private Path myPath;
+    private Paint myPaint;
+    private Boolean isErease = false;
 
-	public void setPaint(Paint p) {
-			myPaint = p;
-	}
+    public PathPaint() {
+        myPath = new Path();
+        myPaint = new Paint();
 
-	public Paint getPaint() {
-			if (isErease) {
-					myPaint.setColor(TiConvert.toColor("black", TiApplication.getAppCurrentActivity()));
-					myPaint.setAlpha(0xFF);
-					myPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-					myPaint.setColor(Color.TRANSPARENT);
-			}
-			return myPaint;
-	}
+        myPaint = new Paint();
+        myPaint.setAntiAlias(true);
+        myPaint.setDither(true);
+        myPaint.setColor(TiConvert.toColor("black", TiApplication.getAppCurrentActivity()));
+        myPaint.setStyle(Paint.Style.STROKE);
+        myPaint.setStrokeJoin(Paint.Join.ROUND);
+        myPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
 
-	public Path getPath() {
-			return myPath;
-	}
+    public Paint getPaint() {
+        if (isErease) {
+            myPaint.setColor(TiConvert.toColor("black", TiApplication.getAppCurrentActivity()));
+            myPaint.setAlpha(0xFF);
+            myPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+            myPaint.setColor(Color.TRANSPARENT);
+        }
+        return myPaint;
+    }
 
-	public void setPath(Path p) {
-			myPath = p;
-	}
+    public void setPaint(Paint p) {
+        myPaint = p;
+    }
 
-	public Boolean getEarase() {
-			return isErease;
-	}
+    public Path getPath() {
+        return myPath;
+    }
 
-	public void setEarase(Boolean p) {
-			isErease = p;
-	}
+    public void setPath(Path p) {
+        myPath = p;
+    }
 
-	public PathPaint() {
-			myPath = new Path();
-			myPaint = new Paint();
+    public Boolean getEarase() {
+        return isErease;
+    }
 
-			myPaint = new Paint();
-			myPaint.setAntiAlias(true);
-			myPaint.setDither(true);
-			myPaint.setColor(TiConvert.toColor("black", TiApplication.getAppCurrentActivity()));
-			myPaint.setStyle(Paint.Style.STROKE);
-			myPaint.setStrokeJoin(Paint.Join.ROUND);
-			myPaint.setStrokeCap(Paint.Cap.ROUND);
-	}
+    public void setEarase(Boolean p) {
+        isErease = p;
+    }
 }
